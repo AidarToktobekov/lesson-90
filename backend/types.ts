8ac8ac6c175fbee45fc6f3a10884e1cc143baa48
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-10-01 13:47:52 +0600

    init app

---
+++ backend/types.ts
@@ -0,0 +1,13 @@
+import {WebSocket} from 'ws';
+
+export interface ActiveConnections {
+    [id: string]: WebSocket
+}
+
+export interface IncomingMessage {
+    type: string;
+    payload: {
+        x: string;
+        y: string
+    };  
+}
